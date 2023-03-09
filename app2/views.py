ebed2b0dd2ab7f2d9b061a9b8a2c82c5510c0304
venkatnulu <psubbaiah77@gmail.com>
2023-03-09 19:48:07 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def djangoguitarist(request):
+    return HttpResponse("your are my favourate guitarist that's why i took your name as my software name")
